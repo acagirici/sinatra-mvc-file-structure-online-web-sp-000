fea0fb400e9d5a50cc1c0f9d4e9c4cbcba5858f3
Arif Cagirici <acagirici23@gmail.com>
2020-12-28 21:57:55 +0000

    Automatically backed up by Learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -2,11 +2,11 @@ class Dog
   attr_accessor :name, :breed, :age
   
   @@all = []
-  def initialize
-    @name=name
-    @breed=breed
-    @age=age
-    @@all << 
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    @@all << self
   end
   
 end
